8ae295436331f84069a51c87def62a11b878466e
aleksandrechia <aleksandrechiaureli@gmail.com>
2024-01-30 01:21:36 +0400

    first commit

---
+++ day 0/lesson1.py
@@ -0,0 +1,83 @@
+from turtle import *
+
+
+# we want to paint a house
+
+ #step 1: draw a spuare    
+
+width(7)
+color("blue")
+begin_fill()
+begin_fill()
+forward(200)
+left(90)
+forward(200)      
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(70)
+left(90)
+end_fill()
+#end of spuare
+
+#drawing a door
+
+color("red")
+begin_fill()
+forward(100)
+right(90)
+forward(50)
+right(90)
+forward(100)
+end_fill()
+#end of door
+
+penup()
+goto(200, 200)
+
+pendown()
+
+color("yellow")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200) 
+end_fill()
+
+penup()
+goto(120, 165)
+pendown()
+
+
+begin_fill()
+left(30)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+end_fill()
+forward(50)
+penup()
+goto(70, 165)
+pendown()
+
+
+begin_fill()
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+end_fill()
+exitonclick()
+
+
+
+
---
+++ day 1/lesson1.py
@@ -0,0 +1,10 @@
+name = "aleqsandre"
+surname = "chiaureli"
+age = 9
+height = 145
+
+
+print("მე მქვია" + " " + name,  " " + surname,  " " + "მე ვარ ",  str(age), "წლის" + " " + "მე ვარ", str(height),  "მეტრი")
+
+
+
