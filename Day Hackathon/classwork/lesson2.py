f4d1e7587fd88ff1576a8150d94f7e3ca3728f46
aleksandrechia <aleksandrechiaureli@gmail.com>
2024-03-24 18:30:53 +0400

    commit

---
+++ Day Hackathon/classwork/lesson2.py
@@ -0,0 +1,27 @@
+from turtle import *
+name = "aleksandre"
+surname = "chiaureli"
+height = 145
+level = 1640
+mom = "nazi"
+dad = "giorgi"
+frend = "dachi"
+
+print(name + " " + surname + " " + str(height) + " " + str(level)+ " " + mom + " " + dad + " " + frend)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
