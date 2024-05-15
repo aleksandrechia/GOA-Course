448b76d7ca41509c0b4007e5cce4b5455992c9dd
aleksandrechia <aleksandrechiaureli@gmail.com>
2024-05-15 20:58:27 +0400

    commit

---
+++ Day 14/Classwork/lesson14.py
@@ -0,0 +1,5 @@
+name = input("enter you name:")
+age = input("enter you age:")
+
+print("hello " + name) 
+print("you age is " + age)
---
+++ Day 14/Homework/lesson14.py
@@ -0,0 +1,19 @@
+friend_name1 = input("enter friend 1 name:")
+friend_name2 = input("enter friend 2 name:")
+friend_name3 = input("enter friend 3 name:")
+friend_name4 = input("enter friend 4 name:")
+
+print("friend name is " + friend_name1)
+print("friend name is " + friend_name2)
+print("friend name is " + friend_name3)
+print("friend name is " + friend_name4)
+
+mom_name = input("enter mom name:")
+dad_name = input("enter dad name:")
+
+print("mom name is " + mom_name)
+print("dad name is " + dad_name)
+
+momxmarebeli = input("enter favorite food:")
+
+print("favorite food " + food)
