3948799dbeb1353e21f465e668d01e73f0df4c6a
aleksandrechia <aleksandrechiaureli@gmail.com>
2024-05-28 00:00:37 +0400

    commit

---
+++ Day 16/Homework/lesson16.py
@@ -0,0 +1,30 @@
+ #შეეკითხეთ მომხმარებელს სახელი, გვარი, ასაკი, მშობლების საელები, გვარები და დამიპრინტეთ ერთად ანუ ამათი concatination მოვახდინოთ ❤️ 
+
+first_name = input(" enter your name:")
+last_name = input(" enter your last name:")
+age = input(" enter your age:")
+parents_name = input(" enter your parents name:")
+parents_last_name = input(" enter your parents last name:")
+
+sentence = first_name + " " + last_name + ", " + age + ", " + parents_name + ", " + parents_last_name
+
+print(sentence)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
