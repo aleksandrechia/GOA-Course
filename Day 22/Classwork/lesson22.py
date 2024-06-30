8f8c125455eb91d6ecdb2c0d7084cf269d864d58
aleksandrechia <aleksandrechiaureli@gmail.com>
2024-06-30 15:47:15 +0400

    commit

---
+++ Day 22/Classwork/lesson22.py
@@ -0,0 +1,4 @@
+str = ("door")
+print((type)("str"))
+float = 10.5
+print((type)("int"))
---
+++ Day 22/Homework/lesson22.py
@@ -0,0 +1,149 @@
+no1 = 100.1
+no2 = "102"
+no3 = 103
+no4 = 100.4
+no5 = "105"
+no6 = 106
+no7 =100.7
+no8 = "108"
+no9 = 109
+no10 =10.10
+no11 = "111"
+no12 = 112
+no13 = 11.13
+no14 = "114"
+no15 = 115
+no16 = 11.16
+no17 = "117"
+no18 = 118
+no19 = 11.19
+no20 = "120"
+no21 = 121
+no22 = 12.12
+no23 = "123"
+no24 = 124
+no25 = 12.15
+no26 = "126"
+no27 = 127
+no28 = 12.18
+no29 = "129"
+no30 = 130
+no31 = 13.10
+no32 = "132"
+no33 = 133
+no34 = 13.14
+no35 = "135"
+no36 = 136
+no37 = 13.17
+no38 = "138"
+no39 = 139
+no40 =14.00
+no41 ="141"
+no42 = 142
+no43 = 14.13
+no44 = "144"
+no45 = 145
+no46 = 14.16
+no47 = "147"
+no48 = 148
+no49 =14.19
+no50 ="150"
+
+print(type(no1))
+print(type(no2))
+print(type(no3))
+print(type(no4))
+print(type(no5))
+print(type(no6))
+print(type(no7))
+print(type(no8))
+print(type(no9))
+print(type(no10))
+print(type(no11))
+print(type(no12))
+print(type(no13))
+print(type(no14))
+print(type(no15))
+print(type(no16))
+print(type(no17))
+print(type(no18))
+print(type(no19))
+print(type(no20))
+print(type(no21))
+print(type(no22))
+print(type(no23))
+print(type(no24))
+print(type(no25))
+print(type(no26))
+print(type(no27))
+print(type(no28))
+print(type(no29))
+print(type(no30))
+print(type(no31))
+print(type(no32))
+print(type(no33))
+print(type(no34))
+print(type(no35))
+print(type(no36))
+print(type(no37))
+print(type(no38))
+print(type(no39))
+print(type(no40))
+print(type(no41))
+print(type(no42))
+print(type(no43))
+print(type(no44))
+print(type(no45))
+print(type(no46))
+print(type(no47))
+print(type(no48))
+print(type(no49))
+print(type(no50))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
